372eeff42b2c413031b471223298b7838ee29ee5
Zhiru <zhiru42@gmail.com>
2021-06-10 01:41:34 -0700

    first draft of close-pair HMM, transition matrix is constrained

--- utils/hmm.py
+++ utils/hmm.py
@@ -1,6 +1,6 @@
 import numpy as np
 from hmmlearn.base import _BaseHMM
-from scipy.stats import poisson
+from scipy.stats import poisson, bernoulli
 
 
 class PoissonHMM(_BaseHMM):
@@ -60,13 +60,88 @@ class PoissonHMM(_BaseHMM):
         super(PoissonHMM, self)._accumulate_sufficient_statistics(
             stats, X, framelogprob, posteriors, fwdlattice, bwdlattice)
         if 'm' in self.params:
-            # TODO: 05.26.2021 figure out whether the next two lines should use posteriors instead of framelogprob!!
-            stats['sum_p'] += np.transpose(np.sum(framelogprob, axis=0))
-            stats['sum_px'] += np.squeeze(np.dot(np.transpose(framelogprob), X))
+            stats['sum_p'] += np.transpose(np.sum(posteriors, axis=0))
+            stats['sum_px'] += np.squeeze(np.dot(np.transpose(posteriors), X))
         return
 
     def _do_mstep(self, stats):
         super(PoissonHMM, self)._do_mstep(stats)
         if 'm' in self.params:
             self.means_ = np.divide(stats['sum_px'], stats['sum_p'])
+        return
+
+
+class ClosePairHMM(_BaseHMM):
+    def __init__(self,
+                 species_name=None, transfer_emissions=np.array([0.1]),
+                 transfer_rate=1e-2, clonal_emission=1e-3,
+                 transfer_length=5e2,
+                 algorithm="viterbi", n_iter=10, tol=1e-2,
+                 verbose=False, params="m"):
+        if species_name is not None:
+            self.transfer_emissions, self.transition_prior = self.get_empirical_emissions(species_name)
+        elif transfer_emissions is not None:
+            self.transfer_emissions = transfer_emissions
+            self.transition_prior = np.ones(transfer_emissions.shape)
+        else:
+            raise ValueError("Must supply either the species name " +
+                             "or the emission probability in transferred regions")
+        n_components = 1 + len(self.transfer_emissions)
+        # normalizing the transition prior
+        self.transition_prior = self.transition_prior.astype(np.float32) / np.sum(self.transition_prior)
+        self.transfer_rate = transfer_rate
+        self.clonal_emission = clonal_emission
+        self.exit_rate = 1. / transfer_length  # rate of leaving the transferred state
+
+        _BaseHMM.__init__(self, n_components,
+                          algorithm=algorithm,
+                          n_iter=n_iter, tol=tol, verbose=verbose,
+                          params=params)
+
+    def get_empirical_emissions(self, species_name):
+        # TODO: implement this!
+        return None, None
+
+    def _init(self, X, lengths=None):
+        init = 1. / self.n_components
+        self.startprob_ = np.zeros(self.n_components)
+        self.startprob_[0] = 1  # always starts from clonal state
+
+        # transmat is very sparse; no transitions between the recombined/transferred states
+        self.transmat_ = np.zeros((self.n_components, self.n_components))
+        # transitions from the recombined state
+        self.transmat_[1:, 0] = self.exit_rate
+        self.transmat_[np.diag_indices(self.n_components)] = 1 - self.exit_rate
+        # transitions from the clonal state
+        self.transmat_[0, 0] = 1 - self.transfer_rate
+        self.transmat_[0, 1:] = self.transfer_rate * self.transition_prior
+
+        _n_samples, n_features = X.shape
+        if n_features != 1:
+            raise ValueError("Only supports binned 1d genome as input data")
+        return
+
+    def _compute_log_likelihood(self, X):
+        # each observation will be either "snp in bin / 1" or "no snp in bin/ 0"
+        # so the emission simply follows bernoulli RV
+        clonal_logp = bernoulli.logpmf(X, self.clonal_emission)
+        transfer_logp = bernoulli.logpmf(X, self.transfer_emissions)
+        # clonal_logp = poisson.logpmf(X, self.clonal_emission)
+        # transfer_logp = poisson.logpmf(X, self.transfer_emissions)
+        logp = np.hstack([clonal_logp, transfer_logp])
+        return logp
+
+    def _initialize_sufficient_statistics(self):
+        # TODO may need to implement for inferring wall clock time
+        return
+
+    def _accumulate_sufficient_statistics(self, stats, X, framelogprob,
+                                          posteriors, fwdlattice, bwdlattice):
+        # TODO may need to implement for inferring wall clock time
+        print("Skipping")
+        return
+
+    def _do_mstep(self, stats):
+        # TODO may need to implement for inferring wall clock time
+        print("skipping m step")
         return
